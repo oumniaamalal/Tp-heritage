1f8067526e1b4289175c606e69a98d730d767d62
pc <pc@htnxy>
2024-11-03 02:12:07 +0100

    fifth commit

---
+++ heritage/src/heritage/PolymorphismeTest.java
@@ -0,0 +1,13 @@
+package heritage;
+
+public class PolymorphismeTest {
+	public static void main(String[] args) {
+	Vehicule voiture = new Voiture();
+    Vehicule velo = new Velo();
+
+        
+        voiture.seDeplacer(); 
+        velo.seDeplacer();
+
+}
+}
